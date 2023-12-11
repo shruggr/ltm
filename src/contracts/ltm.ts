718e0236e5b9eeae366269e345e66d7cd088e397
Dan Wagner <dan.wagner06@gmail.com>
2023-12-10 17:46:27 -0800

    docs: add disclaimer

--- src/contracts/ltm.ts
+++ src/contracts/ltm.ts
@@ -2,24 +2,14 @@
 
 // MIT License
 // Copyright 2023 David Case & Daniel Wagner
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ---
+// Disclaimer:
+// The LTM smart contract (the "Smart Contract") is experimental and has not undergone any formal security audit. Any use of the Smart Contract is at the sole discretion and responsibility of the service employing it. The creators of the Smart Contract, David Case and Daniel Wagner, disclaim any and all liability for damages, losses, or legal consequences arising from the use, deployment, or redemption of the Smart Contract.
+// Users, services, or end users employing the Smart Contract are strongly advised to conduct their own due diligence, seek legal advice, and comply with applicable laws and regulations before utilizing the Smart Contract. By using the Smart Contract, users, services, or end users acknowledge and agree that it is experimental, unaudited, and that they are solely responsible for any risks associated with its use.
+// Any service or end user utilizing the Smart Contract must provide a clear and conspicuous disclaimer stating that the Smart Contract is experimental, unaudited, and that its use involves inherent risks.
 
 import { BSV20V2, Ordinal } from 'scrypt-ord'
 import {
